47be6768bfd1f07de31d0f2354ad40eff1534d5e
Yoshimasa Niwa <niw@niw.at>
2016-03-07 01:24:24 -0800

    Clean up shell script.

--- src/javas.sh
+++ src/javas.sh
@@ -1,6 +1,6 @@
 # javas requires /usr/libexec/java_home to switch java on OS X.
 # See java_home (1).
-if [ ! -x /usr/libexec/java_home ]; then
+if [[ ! -x /usr/libexec/java_home ]]; then
   return 1
 fi
 
@@ -13,13 +13,14 @@ javas() {
     version="1.$version"
     ;;
   esac
+  readonly version
 
-  if [ "x${version}" = "x" ]; then
+  if [[ -z $version ]]; then
     unset JAVA_HOME
   else
     export JAVA_HOME=$(/usr/libexec/java_home -v "$version")
   fi
-  export JAVAS_JAVA_VERSION=$(java -version 2>&1|head -n1|sed -E -e 's/.*"(.*)"/\1/')
+  export JAVAS_JAVA_VERSION=$(java -version 2>&1|awk '/java version ".*"/{gsub(/"/, "", $3); print $3}')
 }
 
 # jvs means javas or java switch.
@@ -30,20 +31,21 @@ _javas_cd_hook() {
   local rcfile
 
   while : ; do
-    if [ -f "$current_dir/.javasrc" ]; then
+    if [[ -f "$current_dir/.javasrc" ]]; then
       rcfile="$current_dir/.javasrc"
       break
     fi
-    if [ -z "$current_dir" \
-      -o "$current_dir" = "$HOME" \
-      -o "$current_dir" = "/" \
-      -o "$current_dir" = "." ]; then
+    if [[ -z "$current_dir" \
+      || "$current_dir" = "$HOME" \
+      || "$current_dir" = "/" \
+      || "$current_dir" = "." ]]; then
       break
     fi
     current_dir="$(dirname "$current_dir")"
   done
+  readonly rcfile
 
-  if [ ! "$javas_LAST_RC_FILE" = "$rcfile" ]; then
+  if [[ ! "$javas_LAST_RC_FILE" = "$rcfile" ]]; then
     # Parse .javasrc
     # In future, this must be yaml or more handy format.
     javas "$(cat "$rcfile"|head -n1|cut -d' ' -f2)"
@@ -52,27 +54,18 @@ _javas_cd_hook() {
 }
 
 enable_javas_cd_hook() {
-  # Force to read javasrc file first.
-  export JAVAS_LAST_RC_FILE=
-  _javas_cd_hook
-
-  # If we could use zsh chpwd_functions, use it.
-  if [ -n "$ZSH_VERSION" ]; then
+  # Only when we can use zsh chpwd_functions, use it.
+  if [[ -n "$ZSH_VERSION" ]]; then
     autoload -Uz is-at-least
     if is-at-least 4.3.4 >/dev/null 2>&1; then
-      typeset -ga chpwd_functions
+      # Force to read javasrc file first.
+      export JAVAS_LAST_RC_FILE=
+      _javas_cd_hook
+
+      typeset -gaU chpwd_functions
       chpwd_functions+=_javas_cd_hook
       return
     fi
   fi
-
-  # Otherwise, overwrite cd.
-  cd() {
-    builtin cd "$@"
-    local result=$?
-    _javas_cd_hook
-    return $result
-  }
+  return 1
 }
-
-# vim:sw=2 ts=2 expandtab:
